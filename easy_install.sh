8ff3ef72002774727d6d4aa053678a0963106b25
stephenreid <stephenreid@ratedsar.com>
2012-12-02 21:14:43 -0500

    Check for python, exit early if not. Check for easy_install, install it if not.

--- easy_install.sh
+++ easy_install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 echo "Installing all of the Python Requirements for QSTK"
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+hash python  &> /dev/null
+if [ $? -eq 1 ]; then
+	echo >&2 "Python is not installed on this system. You must install it first"
+	exit 1
+fi
+
+hash easy_install &> /dev/null
+if [ $? -eq 1 ]; then
+	echo >&2 "Python setup tools: easy_install, not found, downloading and installing"
+
+	wget http://peak.telecommunity.com/dist/ez_setup.py
+	python z_setup.py
+	rm ez_setup.py
+fi
+
 
 easy_install numpy
 easy_install scipy
